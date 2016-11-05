cdb9d3ae584d355d757bbd50e2dca184773d6f8f
Will Pittman <willjpittman@gmail.com>
2016-11-05 13:16:01 -0400

    MINOR: started exploring how Unittesting works with Qt

--- pyflowgraph/tests/test_test.py
+++ pyflowgraph/tests/test_test.py
@@ -1,10 +1,31 @@
 
-from unittest import TestCase
+from Qt.QtTest import QTest
+from Qt import QtWidgets
+from unittest  import TestCase
 import os
+
+
+#QApp = QtWidgets.QApplication(sys.argv)
+
+
+
+class TestButton( QtWidgets.QPushButton ):
+    def __init__(self,*args,**kwds):
+        super( TestButton, self ).__init__(*args,**kwds)
+        self.clicked.connect( self.changetext )
+
+    def changetext(self,event):
+        self.setText( 'changed' )
 
 
 class Test_Testing(TestCase):
     def test_testing(self):
         self.assertEqual( 42, 42 )
+
+    def test_qtest(self):
+        btn = QPushButton('nothing')
+        QTest.mouseClick( btn, QtCore.Qt.LeftButton )
+        self.assertEqual( btn.text(), 'changed')
+
 
 
